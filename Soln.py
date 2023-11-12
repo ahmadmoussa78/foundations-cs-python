2103e3a8b8744c8b84421280005693b31e9b0d63
ahmad moussa <ahmadalhajmoussa78@gmail.com>
2023-11-12 21:19:28 +0200

    Editing switchtab

--- Soln.py
+++ Soln.py
@@ -49,7 +49,7 @@ def openTab():
     print("Invalid input.Please enter an valid index")
 
 def switchTab(num, data):  # worst case =>O(n),Where N is the size of tabs list
-  global tabs
+  
   index = -1 if num == "" else int(num)
 
   if not tabs:
