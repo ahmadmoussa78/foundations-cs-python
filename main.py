24c3266af0724d0a92610900aef9f1dd40f78564
ahmad mousssa <ahmadalhajmoussa78@gmail.com>
2023-11-26 15:34:46 +0200

    creating linkedlistclass

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ class Node:
     self.data = data
     self.next = None
     
+class SinglyLinkedList:
+
+  def __init__(self):
+    self.head = None
+
+  def append(self, data):
+    new_node = Node(data)
+    if not self.head:
+      self.head = new_node
+    else:
+      current = self.head
+      while current.next:
+        current = current.next
+      current.next = new_node
+
 def main():
   name = input("Enter your name: ")
   print(f"Welcome, {name}!")
